7ca68d246717b2ea8fed159d186bac9be07d2ce6
EvgeniyTyupa <boogee2820@gmail.com>
2021-10-11 18:02:36 +0300

    icons partners

--- front/src/Hooks/usePartners.js
+++ front/src/Hooks/usePartners.js
@@ -1,13 +1,26 @@
 import r1 from '../Assets/icons/Partners/r1.svg'
+import crowbar from '../Assets/icons/Partners/CrowBar.png';
+import legenda from '../Assets/icons/Partners/Legenda.png';
+import snapchat from '../Assets/icons/Partners/Snapchat.png';
+import talantika from '../Assets/icons/Partners/Talantika.jpg';
+import union from '../Assets/icons/Partners/UnionGroup1.png';
+import vidnova from '../Assets/icons/Partners/Vidnova.png';
+import basketball from '../Assets/icons/Partners/basketbol.png';
+import gold from '../Assets/icons/Partners/gold.jpg';
+import hata from '../Assets/icons/Partners/hata.png';
 
 export const usePartners = () => {
     let partners = [
         { id: 'r1', img: r1 },
-        { id: 'r1', img: r1 },
-        { id: 'r1', img: r1 },
-        { id: 'r1', img: r1 },
-        { id: 'r1', img: r1 },
-        { id: 'r1', img: r1 },
+        { id: 'crow', img: crowbar },
+        { id: 'legenda', img: legenda },
+        { id: 'snapchat', img: snapchat },
+        { id: 'talantika', img: talantika },
+        { id: 'union', img: union },
+        { id: 'vidnova', img: vidnova },
+        { id: 'basketball', img: basketball },
+        { id: 'gold', img: gold },
+        { id: 'hata', img: hata },
     ]
     return partners
 }
